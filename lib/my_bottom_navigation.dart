fd428ee98e88bf85a2c58e30336a761a2f9c33ed
imaNNeoFighT <iman.neofight@gmail.com>
2020-10-11 01:59:46 +0330

    SS

--- lib/my_bottom_navigation.dart
+++ lib/my_bottom_navigation.dart
@@ -1,49 +1,77 @@
 import 'package:flutter/material.dart';
 
 class MyBottomNavigation extends StatelessWidget {
+  final double fabSize = 62;
+  final double navHeight = 68;
+  final double fabMargin = 8;
 
-  final double curvePercent;
-
-  const MyBottomNavigation({Key key, this.curvePercent}) : super(key: key);
+  const MyBottomNavigation({
+    Key key,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return CustomPaint(
-      painter: _MyBottomNavigationCustomPainter(curvePercent),
-      size: Size(
-        double.infinity,
-        74,
+    return SizedBox(
+      width: double.infinity,
+      height: navHeight + fabSize / 2,
+      child: Stack(
+        children: [
+          CustomPaint(
+            painter: _MyBottomNavigationCustomPainter(0.5, fabSize, fabMargin),
+            size: Size(
+              double.infinity,
+              navHeight + fabSize / 2,
+            ),
+          ),
+          Align(
+            alignment: Alignment.topCenter,
+            child: SizedBox(
+              width: fabSize,
+              height: fabSize,
+              child: FloatingActionButton(
+                onPressed: () {},
+                backgroundColor: Colors.black,
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
 }
 
 class _MyBottomNavigationCustomPainter extends CustomPainter {
-
   final double targetXPercent;
+  final double fabSize;
+  final double fabMargin;
 
-  _MyBottomNavigationCustomPainter(this.targetXPercent);
+  _MyBottomNavigationCustomPainter(
+    this.targetXPercent,
+    this.fabSize,
+    this.fabMargin,
+  );
 
   @override
   void paint(Canvas canvas, Size size) {
-
     double holeWidth = 100;
-    double holeWidthHalf = holeWidth / 2;
-    double holeHeight = 40;
+    double holeWidthThird = (holeWidth / 3) * 2;
+    double holeHeight = fabSize + fabMargin;
+
+    double top = fabSize / 2;
 
     Path p = new Path();
 
     final targetX = size.width * targetXPercent;
 
-    p.moveTo(0, 0);
+    p.moveTo(0, top);
 
-    final point1 = Offset(targetX - holeWidthHalf, 0);
+    final point1 = Offset(targetX - holeWidthThird, top);
     p.lineTo(point1.dx, point1.dy);
 
     final point2 = Offset(targetX, holeHeight);
 
-    final controlPoint1 = Offset(point1.dx + 30, 0);
-    final controlPoint2 = Offset(point1.dx + 18, 40);
+    final controlPoint1 = Offset(point1.dx + 25, top);
+    final controlPoint2 = Offset(point1.dx + 30, holeHeight);
 
     p.cubicTo(
       controlPoint1.dx,
@@ -54,9 +82,9 @@ class _MyBottomNavigationCustomPainter extends CustomPainter {
       point2.dy,
     );
 
-    final point3 = Offset(targetX + holeWidthHalf, 0);
-    final controlPoint3 = Offset(point3.dx - 18, 40);
-    final controlPoint4 = Offset(point3.dx - 30, 0);
+    final point3 = Offset(targetX + holeWidthThird, top);
+    final controlPoint3 = Offset(point3.dx - 30, holeHeight);
+    final controlPoint4 = Offset(point3.dx - 25, top);
 
     p.cubicTo(
       controlPoint3.dx,
@@ -67,17 +95,18 @@ class _MyBottomNavigationCustomPainter extends CustomPainter {
       point3.dy,
     );
 
-    p.lineTo(size.width, 0);
+    p.lineTo(size.width, top);
     p.lineTo(size.width, size.height);
     p.lineTo(0, size.height);
-    p.lineTo(0, 0);
+    p.lineTo(0, top);
+
+    canvas.clipRect(Rect.fromLTWH(0, 0, size.width, size.height));
 
     canvas.drawPath(
-      p,
-      Paint()
-        ..color = Colors.black
-        ..style = PaintingStyle.fill
-        ..strokeWidth = 2);
+        p,
+        Paint()
+          ..color = Colors.black
+          ..style = PaintingStyle.fill);
   }
 
   void drawPoint(Canvas canvas, Offset point) {

847b702c08f676706833877abc9a74d20a87ef4e
imaNNeoFighT <iman.neofight@gmail.com>
2020-10-25 21:08:16 +0330

    Implemented horizontal_snapping_list

---
+++ lib/horizontal_snapping_list.dart
@@ -0,0 +1,109 @@
+import 'package:flutter/material.dart';
+import 'dart:math' as math;
+
+class HorizontalSnappingList extends StatefulWidget {
+  final int itemCount;
+  final IndexedWidgetBuilder itemBuilder;
+  final double itemWidth;
+  final double itemHorizontalMargin;
+  final double listHeight;
+  final double itemsConsumedWidth;
+
+  HorizontalSnappingList({
+    @required this.itemBuilder,
+    this.itemCount,
+    this.itemWidth = 120.0,
+    this.itemHorizontalMargin = 4.0,
+    this.listHeight = 200.0,
+  }) : itemsConsumedWidth = itemWidth + (itemHorizontalMargin * 2);
+
+  @override
+  _HorizontalSnappingListState createState() => _HorizontalSnappingListState();
+}
+
+class _HorizontalSnappingListState extends State<HorizontalSnappingList> {
+  ScrollController _scrollController;
+  double scrollOffset = 0;
+
+  @override
+  void initState() {
+    _scrollController = new ScrollController();
+    _scrollController.addListener(() {
+      setState(() {
+        scrollOffset = _scrollController.position.pixels / widget.itemsConsumedWidth;
+      });
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: widget.listHeight,
+      child: ListView.builder(
+        controller: _scrollController,
+        scrollDirection: Axis.horizontal,
+        physics: SnappingListScrollPhysic(
+            itemWidth: widget.itemWidth + (widget.itemHorizontalMargin * 2)),
+        itemCount: widget.itemCount,
+        itemBuilder: (context, i) {
+          double scale = ((1 - math.min((i - scrollOffset).abs(), 1.0)) * (1 - 0.8)) + 0.8;
+          return Transform.scale(
+            scale: scale,
+            child: widget.itemBuilder(context, i),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class SnappingListScrollPhysic extends ScrollPhysics {
+  final double itemWidth;
+
+  const SnappingListScrollPhysic({
+    @required this.itemWidth,
+    ScrollPhysics parent,
+  }) : super(parent: parent);
+
+  @override
+  SnappingListScrollPhysic applyTo(ScrollPhysics ancestor) => SnappingListScrollPhysic(
+    parent: buildParent(ancestor),
+    itemWidth: itemWidth,
+  );
+
+  double _getItem(ScrollPosition position) => (position.pixels) / itemWidth;
+
+  double _getPixels(ScrollPosition position, double item) =>
+      math.min(item * itemWidth, position.maxScrollExtent);
+
+  double _getTargetPixels(ScrollPosition position, Tolerance tolerance, double velocity) {
+    double item = _getItem(position);
+    if (velocity < -tolerance.velocity) {
+      item -= 0.5;
+    } else if (velocity > tolerance.velocity) {
+      item += 0.5;
+    }
+    return _getPixels(position, item.roundToDouble());
+  }
+
+  @override
+  Simulation createBallisticSimulation(ScrollMetrics position, double velocity) {
+    // If we're out of range and not headed back in range, defer to the parent
+    // ballistics, which should put us back in range at a page boundary.
+    if ((velocity <= 0.0 && position.pixels <= position.minScrollExtent) ||
+        (velocity >= 0.0 && position.pixels >= position.maxScrollExtent)) {
+      return super.createBallisticSimulation(position, velocity);
+    }
+    final Tolerance tolerance = this.tolerance;
+    final double target = _getTargetPixels(position, tolerance, velocity);
+    if (target != position.pixels) {
+      return ScrollSpringSimulation(spring, position.pixels, target, velocity,
+          tolerance: tolerance);
+    }
+    return null;
+  }
+
+  @override
+  bool get allowImplicitScrolling => false;
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,144 +1,47 @@
+import 'package:flutter/material.dart';
 import 'dart:math' as math;
 
-import 'package:flutter/material.dart';
-import 'package:ui_challenge_2/my_bottom_navigation.dart';
+import 'horizontal_snapping_list.dart';
 
 main() => runApp(MaterialApp(home: MyHomePage()));
 
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
+class MyHomePage extends StatelessWidget {
 
-class _MyHomePageState extends State<MyHomePage> {
-  ScrollController _controller;
-  int firstItem = 0;
-  final double itemWidth = 128;
-
-  final ValueNotifier<double> _offsett = ValueNotifier<double>(0.0);
-
-  double offset = 0;
-
-  @override
-  void initState() {
-    _controller = new ScrollController();
-    _controller.addListener(() {
-      setState(() {
-        offset = _controller.position.pixels / itemWidth;
-        firstItem = _controller.position.pixels ~/ itemWidth;
-      });
-    });
-    super.initState();
-  }
+  final double itemWidth = 120.0;
+  final double itemHeight = 200.0;
+  final double horizontalItemMargin = 4.0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Carousel in vertical scrollable'),
-      ),
-      bottomNavigationBar: MyBottomNavigation(
-        selected: 1,
-        items: [
-          Icon(Icons.home, color: Colors.white,),
-          Icon(Icons.home, color: Colors.white,),
-          Icon(Icons.home, color: Colors.white,),
-          Icon(Icons.home, color: Colors.white,),
-          Icon(Icons.home, color: Colors.white,),
-        ],
+        title: Text('Flutter 4 Fun'),
       ),
       body: Center(
-        child: SizedBox(
-          // you may want to use an aspect ratio here for tablet support
-          height: 200.0,
-          child: ListView.builder(
-            controller: _controller,
-            physics: SnappingListScrollPhysics(itemWidth: itemWidth),
-            scrollDirection: Axis.horizontal,
-            itemBuilder: (context, i) {
-              double othersScale = 0.8;
-              double scale = ((1 - math.min((i - offset).abs(), 1.0)) * (1 - othersScale)) + othersScale;
-              print('i: $i, scale: $scale');
-              return Transform.scale(
-                scale: scale,
-                child: _buildCarouselItem(context, 0, i),
-              );
-            },
+        child: HorizontalSnappingList(
+          itemWidth: 120,
+          itemHorizontalMargin: horizontalItemMargin,
+          itemBuilder: (context, i) => Container(
+            width: itemWidth,
+            height: itemHeight,
+            margin: EdgeInsets.symmetric(horizontal: horizontalItemMargin),
+            decoration: BoxDecoration(
+              color: Colors.purple,
+              borderRadius: BorderRadius.all(Radius.circular(4.0)),
+            ),
+            child: Center(
+              child: Text(
+                '$i',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 22,
+                ),
+              ),
+            ),
           ),
         ),
       ),
     );
   }
-
-  Widget _buildCarouselItem(BuildContext context, int carouselIndex, int itemIndex) {
-    return Container(
-      width: 120,
-      height: 200,
-      margin: EdgeInsets.symmetric(horizontal: 4.0),
-      decoration: BoxDecoration(
-        color: Colors.purple,
-        borderRadius: BorderRadius.all(Radius.circular(4.0)),
-      ),
-      child: Center(
-        child: Text(
-          '$itemIndex',
-          style: TextStyle(
-            color: Colors.white,
-            fontWeight: FontWeight.bold,
-            fontSize: 22,
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class SnappingListScrollPhysics extends ScrollPhysics {
-  final double itemWidth;
-
-  const SnappingListScrollPhysics({
-    @required this.itemWidth,
-    ScrollPhysics parent,
-  }) : super(parent: parent);
-
-  @override
-  SnappingListScrollPhysics applyTo(ScrollPhysics ancestor) => SnappingListScrollPhysics(
-    parent: buildParent(ancestor),
-    itemWidth: itemWidth,
-  );
-
-  double _getItem(ScrollPosition position) => (position.pixels) / itemWidth;
-
-  double _getPixels(ScrollPosition position, double item) =>
-    math.min(item * itemWidth, position.maxScrollExtent);
-
-  double _getTargetPixels(ScrollPosition position, Tolerance tolerance, double velocity) {
-    double item = _getItem(position);
-    if (velocity < -tolerance.velocity) {
-      item -= 0.5;
-    } else if (velocity > tolerance.velocity) {
-      item += 0.5;
-    }
-    return _getPixels(position, item.roundToDouble());
-  }
-
-  @override
-  Simulation createBallisticSimulation(ScrollMetrics position, double velocity) {
-    // If we're out of range and not headed back in range, defer to the parent
-    // ballistics, which should put us back in range at a page boundary.
-    if ((velocity <= 0.0 && position.pixels <= position.minScrollExtent) ||
-      (velocity >= 0.0 && position.pixels >= position.maxScrollExtent)) {
-      return super.createBallisticSimulation(position, velocity);
-    }
-    final Tolerance tolerance = this.tolerance;
-    final double target = _getTargetPixels(position, tolerance, velocity);
-    if (target != position.pixels) {
-      return ScrollSpringSimulation(spring, position.pixels, target, velocity,
-        tolerance: tolerance);
-    }
-    return null;
-  }
-
-  @override
-  bool get allowImplicitScrolling => false;
 }

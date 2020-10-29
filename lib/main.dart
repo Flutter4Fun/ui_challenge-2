478736739f75473efc5d869b8fc0ed7774fc997b
imaNNeoFighT <iman.neofight@gmail.com>
2020-10-29 20:17:40 +0330

    added curved_bottom_navigation library

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
+import 'package:curved_bottom_navigation/curved_bottom_navigation.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:ui_challenge_2/new_games_section.dart';
 import 'package:ui_challenge_2/stories_section.dart';
-import 'curved_bottom_navigation.dart';
 import 'heading_section.dart';
 import 'popular_games_section.dart';
 import 'recommended_games_section.dart';
